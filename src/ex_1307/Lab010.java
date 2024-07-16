b3d741fbd7630837b27fb06763146d24113e4742
ragotham.deekshitha <rdeekshareddy5856@gmail.com>
2024-07-16 15:53:02 +0530

    initial commit

---
+++ src/ex_1307/Lab010.java
@@ -0,0 +1,22 @@
+package ex_1307;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        String s="Deekshitha";
+        String s1=s.toLowerCase();
+        String s2=s.toUpperCase();
+        int s3= s.length();
+        int s4=s.indexOf('s');
+        String s5=s.substring(0,4);
+        boolean s6=s.equals(s2);
+        boolean s7=s.equalsIgnoreCase(s2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+        System.out.println(s7);
+
+    }
+}
